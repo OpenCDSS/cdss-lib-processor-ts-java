82cd0bfcecc2cc85a0c53cd84daa9db02d666551
Steve Malers <steve.malers@openwaterfoundation.org>
2007-12-28 06:12:48 +0000

    Add RunJython() command, with supporthing Jython external jar.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSCommandProcessor/java_142/trunk@70 84f94280-d8e6-414f-a583-91c4d82d5b43

---
+++ src/rti/tscommandprocessor/commands/util/RunPython_Command.java
@@ -0,0 +1,247 @@
+package rti.tscommandprocessor.commands.util;
+
+import java.io.File;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+
+import org.python.util.PythonInterpreter; 
+import org.python.core.PyException;
+//import org.python.core.PyInteger;
+//import org.python.core.PyObject;
+
+import RTi.Util.Message.Message;
+import RTi.Util.Message.MessageUtil;
+import RTi.Util.IO.AbstractCommand;
+import RTi.Util.IO.Command;
+import RTi.Util.IO.CommandException;
+import RTi.Util.IO.CommandLogRecord;
+import RTi.Util.IO.CommandPhaseType;
+import RTi.Util.IO.CommandProcessor;
+import RTi.Util.IO.CommandStatus;
+import RTi.Util.IO.CommandStatusType;
+import RTi.Util.IO.CommandWarningException;
+import RTi.Util.IO.InvalidCommandParameterException;
+import RTi.Util.IO.IOUtil;
+import RTi.Util.IO.PropList;
+
+import rti.tscommandprocessor.core.TSCommandProcessorUtil;
+
+/**
+<p>
+This class initializes, checks, and runs the RunPython() command.
+</p>
+*/
+public class RunPython_Command extends AbstractCommand implements Command
+{
+    
+/**
+Indicate whether Jython has been initialized.
+*/
+private static boolean __Jython_initialized = false;
+
+/**
+Constructor.
+*/
+public RunPython_Command ()
+{	super();
+	setCommandName ( "RunPython" );
+}
+
+/**
+Check the command parameter for valid values, combination, etc.
+@param parameters The parameters for the command.
+@param command_tag an indicator to be used when printing messages, to allow a
+cross-reference to the original commands.
+@param warning_level The warning level to use when printing parse warnings
+(recommended is 2 for initialization, and 1 for interactive command editor
+dialogs).
+*/
+public void checkCommandParameters ( PropList parameters, String command_tag, int warning_level )
+throws InvalidCommandParameterException
+{	String InputFile = parameters.getValue ( "InputFile" );
+	String warning = "";
+    String message;
+	
+	CommandProcessor processor = getCommandProcessor();
+    CommandStatus status = getCommandStatus();
+    status.clearLog(CommandPhaseType.INITIALIZATION);
+	
+	if ( (InputFile == null) || (InputFile.length() == 0) ) {
+        message = "The input file must be specified.";
+		warning += "\n" + message;
+        status.addToLog ( CommandPhaseType.INITIALIZATION,
+                new CommandLogRecord(CommandStatusType.FAILURE,
+                        message, "Specify an input file." ) );
+	}
+	else {	String working_dir = null;
+	
+			try { Object o = processor.getPropContents ( "WorkingDir" );
+					// Working directory is available so use it...
+					if ( o != null ) {
+						working_dir = (String)o;
+					}
+			}
+			catch ( Exception e ) {
+				message = "Error requesting WorkingDir from processor.";
+                warning += "\n" + message;
+                status.addToLog ( CommandPhaseType.INITIALIZATION,
+                        new CommandLogRecord(CommandStatusType.FAILURE,
+                                message, "Software error - report problem to support." ) );
+			}
+	
+		try {
+            String adjusted_path = IOUtil.verifyPathForOS(IOUtil.adjustPath ( working_dir, InputFile));
+			File f = new File ( adjusted_path );
+			if ( !f.exists() ) {
+                message = "The input file does not exist: \"" + adjusted_path + "\".";
+				warning += "\n" + message;
+                status.addToLog ( CommandPhaseType.INITIALIZATION,
+                        new CommandLogRecord(CommandStatusType.FAILURE,
+                                message, "Verify that the command file to run exists." ) );
+            }
+			f = null;
+		}
+		catch ( Exception e ) {
+            message = "The input file \"" + InputFile +
+            "\" ncannot be adjusted to an absolute path using the working directory \"" +
+            working_dir + "\".";
+			warning += "\n" + message;
+            status.addToLog ( CommandPhaseType.INITIALIZATION,
+                    new CommandLogRecord(CommandStatusType.FAILURE,
+                            message, "Verify that command file to run and working directory paths are compatible." ) );
+		}
+	}
+
+	// Check for invalid parameters...
+	Vector valid_Vector = new Vector();
+	valid_Vector.add ( "InputFile" );
+    warning = TSCommandProcessorUtil.validateParameterNames ( valid_Vector, this, warning );
+
+	if ( warning.length() > 0 ) {
+		Message.printWarning ( warning_level,
+		MessageUtil.formatMessageTag(command_tag,warning_level),
+		warning );
+		throw new InvalidCommandParameterException ( warning );
+	}
+    status.refreshPhaseSeverity(CommandPhaseType.INITIALIZATION,CommandStatusType.SUCCESS);
+}
+
+/**
+Edit the command.
+@param parent The parent JFrame to which the command dialog will belong.
+@return true if the command was edited (e.g., "OK" was pressed), and false if
+not (e.g., "Cancel" was pressed.
+*/
+public boolean editCommand ( JFrame parent )
+{	// The command will be modified if changed...
+	return (new RunPython_JDialog ( parent, this )).ok();
+}
+
+/**
+Run the command.
+@param command_number Number of command in sequence.
+@exception CommandWarningException Thrown if non-fatal warnings occur (the
+command could produce some results).
+@exception CommandException Thrown if fatal warnings occur (the command could
+not produce output).
+*/
+public void runCommand ( int command_number )
+throws InvalidCommandParameterException,
+CommandWarningException, CommandException
+{	String routine = "RunCommands_Command.runCommand", message;
+	int warning_count = 0;
+	int warning_level = 2;
+	String command_tag = "" + command_number;
+	
+	CommandProcessor processor = getCommandProcessor();
+    CommandStatus status = getCommandStatus();
+    status.clearLog(CommandPhaseType.RUN);
+	PropList parameters = getCommandParameters();
+
+	String InputFile = parameters.getValue ( "InputFile" );
+	
+	if ( warning_count > 0 ) {
+		message = "There were " + warning_count + " warnings about command parameters.";
+		Message.printWarning ( warning_level, 
+		MessageUtil.formatMessageTag(command_tag, ++warning_count),
+		routine, message );
+		throw new InvalidCommandParameterException ( message );
+	}
+	
+	// Get the working directory from the processor that is running the commands.
+
+	String WorkingDir = TSCommandProcessorUtil.getWorkingDir(processor);
+	String InputFile_full = null;
+	try {
+        InputFile_full = IOUtil.verifyPathForOS(IOUtil.adjustPath ( WorkingDir, InputFile) );
+		Message.printStatus ( 2, routine,
+		"Processing Python script file \"" + InputFile_full + "\" using command file runner.");
+        
+		runPythonScript ( InputFile_full, WorkingDir );
+
+		Message.printStatus ( 2, routine,"...done processing Python file." );
+	}
+	catch ( Exception e ) {
+		Message.printWarning ( 3, routine, e );
+		message = "Error processing Python file \"" + InputFile_full + "\".";
+		Message.printWarning ( warning_level, 
+		MessageUtil.formatMessageTag(command_tag, ++warning_count),
+		routine, message );
+		throw new CommandException ( message );
+	}
+    status.refreshPhaseSeverity(CommandPhaseType.RUN,CommandStatusType.SUCCESS);
+}
+
+/**
+Run the Jython script.
+*/
+private void runPythonScript ( String pyfile, String WorkingDir )
+throws PyException
+{
+    // Only need to do this once...
+    if ( !__Jython_initialized ) {
+        // This passes to the interpreter the environment information from the application
+        // Tell the script the working directory.
+        String [] args = new String[1];
+        args[0] = WorkingDir;
+        PythonInterpreter.initialize(System.getProperties(), null, args );
+    }
+    PythonInterpreter interp = new PythonInterpreter();
+
+    interp.execfile ( pyfile );
+    /*
+    System.out.println("Hello, brave new world");
+    interp.exec("import sys");
+    interp.exec("print sys");
+
+    interp.set("a", new PyInteger(42));
+    interp.exec("print a");
+    interp.exec("x = 2+2");
+    PyObject x = interp.get("x");
+
+    System.out.println("x: "+x);
+    System.out.println("Goodbye, cruel world");
+    */
+
+}
+
+/**
+Return the string representation of the command.
+*/
+public String toString ( PropList props )
+{	if ( props == null ) {
+		return getCommandName() + "()";
+	}
+	String InputFile = props.getValue("InputFile");
+	StringBuffer b = new StringBuffer ();
+	if ( (InputFile != null) && (InputFile.length() > 0) ) {
+		if ( b.length() > 0 ) {
+			b.append ( "," );
+		}
+		b.append ( "InputFile=\"" + InputFile + "\"" );
+	}
+	return getCommandName() + "(" + b.toString() + ")";
+}
+
+}

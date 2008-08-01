e036dc60dddbcaf9f10df8a773f32a456715591b
Steve Malers <steve.malers@openwaterfoundation.org>
2008-08-01 00:14:39 +0000

    Convert SetAutoExtendPeriod to command class.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSCommandProcessor/java_142/trunk@176 84f94280-d8e6-414f-a583-91c4d82d5b43

---
+++ src/rti/tscommandprocessor/commands/ts/SetAutoExtendPeriod_JDialog.java
@@ -0,0 +1,325 @@
+package rti.tscommandprocessor.commands.ts;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import RTi.Util.GUI.JGUIUtil;
+import RTi.Util.GUI.SimpleJButton;
+import RTi.Util.GUI.SimpleJComboBox;
+import RTi.Util.IO.Command;
+import RTi.Util.IO.PropList;
+import RTi.Util.Message.Message;
+
+/**
+Editor dialog for the SetAutoExtendPeriod() command.
+*/
+public class SetAutoExtendPeriod_JDialog extends JDialog
+implements ActionListener, ItemListener, KeyListener, WindowListener
+{
+
+private SimpleJButton	__cancel_JButton = null,// Cancel Button
+			__ok_JButton = null;	// Ok Button
+private SetAutoExtendPeriod_Command __command = null;
+private JTextArea	__command_JTextArea = null;
+private SimpleJComboBox	__AutoExtendPeriod_JComboBox = null;	// Field for true/false
+private boolean		__error_wait = false;
+private boolean		__first_time = true;
+private boolean     __ok = false;       // Indicates whether OK button has been pressed.
+
+/**
+Command editor dialog constructor.
+@param parent JFrame class instantiating this class.
+@param command Command to edit.
+*/
+public SetAutoExtendPeriod_JDialog (JFrame parent, Command command)
+{	super(parent, true);
+	initialize ( parent, command );
+}
+
+/**
+Responds to ActionEvents.
+@param event ActionEvent object
+*/
+public void actionPerformed( ActionEvent event )
+{	Object o = event.getSource();
+
+	if ( o == __cancel_JButton ) {
+		response ( false );
+	}
+	else if ( o == __ok_JButton ) {
+		refresh ();
+		checkInput();
+		if ( !__error_wait ) {
+			response ( true );
+		}
+	}
+}
+
+/**
+Check the input.  If errors exist, warn the user and set the __error_wait flag
+to true.  This should be called before response() is allowed to complete.
+*/
+private void checkInput ()
+{   // Put together a list of parameters to check...
+    PropList props = new PropList ( "" );
+    String AutoExtendPeriod = __AutoExtendPeriod_JComboBox.getSelected();
+    __error_wait = false;
+
+    if ( AutoExtendPeriod.length() > 0 ) {
+        props.set ( "AutoExtendPeriod", AutoExtendPeriod );
+    }
+    try {
+        // This will warn the user...
+        __command.checkCommandParameters ( props, null, 1 );
+    }
+    catch ( Exception e ) {
+        // The warning would have been printed in the check code.
+        __error_wait = true;
+    }
+}
+
+/**
+Commit the edits to the command.  In this case the command parameters have
+already been checked and no errors were detected.
+*/
+private void commitEdits ()
+{   String AutoExtendPeriod = __AutoExtendPeriod_JComboBox.getSelected();
+    __command.setCommandParameter ( "AutoExtendPeriod", AutoExtendPeriod );
+}
+
+/**
+Free memory for garbage collection.
+*/
+protected void finalize ()
+throws Throwable
+{	__cancel_JButton = null;
+	__command_JTextArea = null;
+	__command = null;
+	__ok_JButton = null;
+	__AutoExtendPeriod_JComboBox = null;
+	super.finalize ();
+}
+
+/**
+Instantiates the GUI components.
+@param parent JFrame class instantiating this class.
+@param command Command to edit.
+*/
+private void initialize ( JFrame parent, Command command )
+{	__command = (SetAutoExtendPeriod_Command)command;
+
+	addWindowListener( this );
+
+    Insets insetsTLBR = new Insets(2,2,2,2);
+
+	// Main panel...
+
+	JPanel main_JPanel = new JPanel();
+	main_JPanel.setLayout( new GridBagLayout() );
+	getContentPane().add ( "North", main_JPanel );
+	int y = 0;
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel (
+        "This command controls whether the the time series period automatically is extended " +
+        "to match the output period when reading data." ),
+        0, y, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+	
+    JGUIUtil.addComponent(main_JPanel, new JLabel (
+		"Setting the option to True results in the time series period" +
+		" automatically being extended" ),
+		0, ++y, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel (
+		"to include the output period, if SetOutputPeriod() is used prior to this command." ),
+		0, ++y, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+        
+    JGUIUtil.addComponent(main_JPanel, new JLabel (
+        "The default if SetAutoExtendPeriod() is not used is True, " +
+        "allowing for data filling and manipulation for the output period."),
+		0, ++y, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel (
+		"Set the option to False to improve performance, when data " +
+		"filling and manipulation are not needed on the extended period." ),
+		0, ++y, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel ( "Automatically extend period?:" ), 
+		0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+	__AutoExtendPeriod_JComboBox = new SimpleJComboBox ( false );
+	__AutoExtendPeriod_JComboBox.add ( __command._True );
+	__AutoExtendPeriod_JComboBox.add ( __command._False );
+	__AutoExtendPeriod_JComboBox.addItemListener ( this );
+        JGUIUtil.addComponent(main_JPanel, __AutoExtendPeriod_JComboBox,
+		1, y, 1, 1, 1, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel ( "Command:"),
+        0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+    __command_JTextArea = new JTextArea ( 4, 40 );
+    __command_JTextArea.setLineWrap ( true );
+    __command_JTextArea.setWrapStyleWord ( true );
+    __command_JTextArea.setEditable ( false );
+    JGUIUtil.addComponent(main_JPanel, new JScrollPane(__command_JTextArea),
+        1, y, 6, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+	// Refresh the contents...
+	refresh ();
+
+	// South Panel: North
+	JPanel button_JPanel = new JPanel();
+	button_JPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JGUIUtil.addComponent(main_JPanel, button_JPanel, 
+		0, ++y, 8, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+
+	__cancel_JButton = new SimpleJButton("Cancel", this);
+	button_JPanel.add ( __cancel_JButton );
+	__ok_JButton = new SimpleJButton("OK", this);
+	button_JPanel.add ( __ok_JButton );
+
+    setTitle ( "Edit " + __command.getCommandName() + "() Command" );
+	setResizable ( true );
+    pack();
+    JGUIUtil.center( this );
+    super.setVisible( true );
+}
+
+/**
+Handle ItemEvent events.
+@param e ItemEvent to handle.
+*/
+public void itemStateChanged ( ItemEvent e )
+{	refresh();
+}
+
+/**
+Respond to KeyEvents.
+*/
+public void keyPressed ( KeyEvent event )
+{	int code = event.getKeyCode();
+
+	if ( code == KeyEvent.VK_ENTER ) {
+		refresh ();
+		checkInput();
+		if ( !__error_wait ) {
+			response ( true );
+		}
+	}
+}
+
+public void keyReleased ( KeyEvent event )
+{	refresh();
+}
+
+public void keyTyped ( KeyEvent event ) {;}
+
+/**
+Indicate if the user pressed OK (cancel otherwise).
+@return true if the edits were committed, false if the user cancelled.
+*/
+public boolean ok ()
+{   return __ok;
+}
+
+/**
+Refresh the command from the other text field contents.
+*/
+private void refresh ()
+{   String routine = getClass().getName() + ".refresh";
+    String AutoExtendPeriod = "";
+    PropList props = __command.getCommandParameters();
+    if ( __first_time ) {
+        __first_time = false;
+        // Get the parameters from the command...
+        AutoExtendPeriod = props.getValue ( "AutoExtendPeriod" );
+        if ( AutoExtendPeriod == null ) {
+            // Select default...
+            __AutoExtendPeriod_JComboBox.select ( 0 );
+        }
+        else {
+            if ( JGUIUtil.isSimpleJComboBoxItem( __AutoExtendPeriod_JComboBox,AutoExtendPeriod, JGUIUtil.NONE, null, null ) ) {
+                __AutoExtendPeriod_JComboBox.select ( AutoExtendPeriod );
+            }
+            else {
+                Message.printWarning ( 1, routine,
+                "Existing command references an invalid\nAutoExtendPeriod value \"" + AutoExtendPeriod +
+                "\".  Select a different value or Cancel.");
+                __error_wait = true;
+            }
+        }
+    }
+    // Regardless, reset the command from the fields...
+    AutoExtendPeriod = __AutoExtendPeriod_JComboBox.getSelected();
+    props = new PropList ( __command.getCommandName() );
+    props.add ( "AutoExtendPeriod=" + AutoExtendPeriod );
+    __command_JTextArea.setText( __command.toString ( props ) );
+}
+
+/**
+React to the user response.
+@param ok if false, then the edit is cancelled.  If true, the edit is committed
+and the dialog is closed.
+*/
+private void response ( boolean ok )
+{   __ok = ok;  // Save to be returned by ok()
+    if ( ok ) {
+        // Commit the changes...
+        commitEdits ();
+        if ( __error_wait ) {
+            // Not ready to close out!
+            return;
+        }
+    }
+    // Now close out...
+    setVisible( false );
+    dispose();
+}
+
+/**
+Responds to WindowEvents.
+@param event WindowEvent object
+*/
+public void windowClosing( WindowEvent event )
+{	response ( false );
+}
+
+public void windowActivated( WindowEvent evt )
+{
+}
+
+public void windowClosed( WindowEvent evt )
+{
+}
+
+public void windowDeactivated( WindowEvent evt )
+{
+}
+
+public void windowDeiconified( WindowEvent evt )
+{
+}
+
+public void windowIconified( WindowEvent evt )
+{
+}
+
+public void windowOpened( WindowEvent evt )
+{
+}
+
+}

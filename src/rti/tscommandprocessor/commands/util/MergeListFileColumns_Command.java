4a3a9bc423ce62bb7ba0154dd328b4b5433d33e9
Steve Malers <steve.malers@openwaterfoundation.org>
2010-09-15 23:20:29 +0000

    Add default value for new column.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSCommandProcessor/java_142/trunk@613 84f94280-d8e6-414f-a583-91c4d82d5b43

--- src/rti/tscommandprocessor/commands/util/MergeListFileColumns_Command.java
+++ src/rti/tscommandprocessor/commands/util/MergeListFileColumns_Command.java
@@ -428,7 +428,7 @@ throws InvalidCommandParameterException, CommandWarningException, CommandExcepti
 	
 	// Add a column to the table.  For now always treat as a string...
 
-	table.addField ( new TableField(TableField.DATA_TYPE_STRING,NewColumnName) );
+	table.addField ( new TableField(TableField.DATA_TYPE_STRING,NewColumnName), "" );
 
 	// Loop through the table records, merge the columns and set in the new column...
 

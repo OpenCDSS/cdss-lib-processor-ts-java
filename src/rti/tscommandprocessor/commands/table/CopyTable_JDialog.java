d0f2204b7b11c5182bc2a3c67d06d76f7878c010
Steve Malers <steve.malers@openwaterfoundation.org>
2010-09-28 17:09:43 +0000

    Add new CopyTable() command
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSCommandProcessor/java_142/trunk@621 84f94280-d8e6-414f-a583-91c4d82d5b43

---
+++ src/rti/tscommandprocessor/commands/table/CopyTable_JDialog.java
@@ -0,0 +1,352 @@
+package rti.tscommandprocessor.commands.table;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import java.util.List;
+
+import RTi.Util.GUI.JGUIUtil;
+import RTi.Util.GUI.SimpleJButton;
+import RTi.Util.GUI.SimpleJComboBox;
+import RTi.Util.IO.PropList;
+import RTi.Util.Message.Message;
+
+public class CopyTable_JDialog extends JDialog
+implements ActionListener, ItemListener, KeyListener, WindowListener
+{
+
+private boolean __error_wait = false; // To track errors
+private boolean __first_time = true; // Indicate first time display
+private JTextArea __command_JTextArea = null;
+private SimpleJComboBox __TableID_JComboBox = null;
+private JTextField __NewTableID_JTextField = null;
+private JTextField __IncludeColumns_JTextField = null;
+private SimpleJButton __cancel_JButton = null;
+private SimpleJButton __ok_JButton = null;
+private CopyTable_Command __command = null;
+private boolean __ok = false;
+
+/**
+Command dialog constructor.
+@param parent JFrame class instantiating this class.
+@param command Command to edit.
+@param runnable If true, the command can be run from the dialog, as a Tool.
+*/
+public CopyTable_JDialog ( JFrame parent, CopyTable_Command command, List<String> tableIDChoices )
+{	super(parent, true);
+	initialize ( parent, command, tableIDChoices );
+}
+
+/**
+Responds to ActionEvents.
+@param event ActionEvent object
+*/
+public void actionPerformed(ActionEvent event)
+{	Object o = event.getSource();
+
+    if ( o == __cancel_JButton ) {
+		response ( false );
+	}
+	else if ( o == __ok_JButton ) {
+		refresh ();
+		checkInput ();
+		if ( !__error_wait ) {
+			// Command has been edited...
+			response ( true );
+		}
+	}
+}
+
+/**
+Check the input.  If errors exist, warn the user and set the __error_wait flag
+to true.  This should be called before response() is allowed to complete.
+*/
+private void checkInput ()
+{	// Put together a list of parameters to check...
+	PropList props = new PropList ( "" );
+	String TableID = __TableID_JComboBox.getSelected();
+    String NewTableID = __NewTableID_JTextField.getText().trim();
+	String IncludeColumns = __IncludeColumns_JTextField.getText().trim();
+	__error_wait = false;
+
+    if ( TableID.length() > 0 ) {
+        props.set ( "TableID", TableID );
+    }
+    if ( NewTableID.length() > 0 ) {
+        props.set ( "NewTableID", NewTableID );
+    }
+	if ( IncludeColumns.length() > 0 ) {
+		props.set ( "IncludeColumns", IncludeColumns );
+	}
+	try {
+	    // This will warn the user...
+		__command.checkCommandParameters ( props, null, 1 );
+	}
+	catch ( Exception e ) {
+        Message.printWarning(2,"", e);
+		// The warning would have been printed in the check code.
+		__error_wait = true;
+	}
+}
+
+/**
+Commit the edits to the command.  In this case the command parameters have
+already been checked and no errors were detected.
+*/
+private void commitEdits ()
+{	String TableID = __TableID_JComboBox.getSelected();
+    String NewTableID = __NewTableID_JTextField.getText().trim();
+    String IncludeColumns = __IncludeColumns_JTextField.getText().trim();
+    __command.setCommandParameter ( "TableID", TableID );
+    __command.setCommandParameter ( "NewTableID", NewTableID );
+	__command.setCommandParameter ( "IncludeColumns", IncludeColumns );
+}
+
+/**
+Free memory for garbage collection.
+*/
+protected void finalize ()
+throws Throwable
+{	__IncludeColumns_JTextField = null;
+	__cancel_JButton = null;
+	__command_JTextArea = null;
+	__command = null;
+	__ok_JButton = null;
+	super.finalize ();
+}
+
+/**
+Instantiates the GUI components.
+@param parent JFrame class instantiating this class.
+@param command Command to edit and possibly run.
+*/
+private void initialize ( JFrame parent, CopyTable_Command command, List<String> tableIDChoices )
+{	__command = command;
+
+	addWindowListener(this);
+
+    Insets insetsTLBR = new Insets(2,2,2,2);
+
+	// Main panel...
+
+	JPanel main_JPanel = new JPanel();
+	main_JPanel.setLayout(new GridBagLayout());
+	getContentPane().add ("North", main_JPanel);
+	int y = 0;
+
+	JPanel paragraph = new JPanel();
+	paragraph.setLayout(new GridBagLayout());
+	int yy = 0;
+    
+   	JGUIUtil.addComponent(paragraph, new JLabel (
+        "This command creates a new table by copying another table."),
+        0, yy, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.BOTH, GridBagConstraints.WEST);
+    JGUIUtil.addComponent(paragraph, new JLabel (
+        "For example, single column tables can be created from a larger table to use as a list."),
+        0, ++yy, 7, 1, 0, 0, insetsTLBR, GridBagConstraints.BOTH, GridBagConstraints.WEST);
+
+	JGUIUtil.addComponent(main_JPanel, paragraph,
+		0, y, 7, 1, 0, 0, 5, 0, 10, 0, GridBagConstraints.NONE, GridBagConstraints.WEST);
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel ( "Table ID:" ), 
+        0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+    __TableID_JComboBox = new SimpleJComboBox ( 12, true );    // Allow edit
+    tableIDChoices.add(0,""); // Add blank to ignore table
+    __TableID_JComboBox.setData ( tableIDChoices );
+    __TableID_JComboBox.addItemListener ( this );
+    //__TableID_JComboBox.setMaximumRowCount(tableIDChoices.size());
+    JGUIUtil.addComponent(main_JPanel, __TableID_JComboBox,
+        1, y, 2, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+    JGUIUtil.addComponent(main_JPanel, new JLabel( "Required - original table."), 
+        3, y, 4, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+	
+    JGUIUtil.addComponent(main_JPanel, new JLabel ("New table ID:"),
+        0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+    __NewTableID_JTextField = new JTextField (10);
+    __NewTableID_JTextField.addKeyListener (this);
+    JGUIUtil.addComponent(main_JPanel, __NewTableID_JTextField,
+        1, y, 2, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+    JGUIUtil.addComponent(main_JPanel, new JLabel ("Required - unique identifier for the new table."),
+        3, y, 4, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST );
+    
+    JGUIUtil.addComponent(main_JPanel, new JLabel ("Column names to copy:"), 
+        0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+    __IncludeColumns_JTextField = new JTextField (10);
+    __IncludeColumns_JTextField.addKeyListener ( this );
+    JGUIUtil.addComponent(main_JPanel, new JScrollPane(__IncludeColumns_JTextField),
+        1, y, 2, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+    JGUIUtil.addComponent(main_JPanel, new JLabel ("Optional - names of columns to copy (default=copy all)."),
+        3, y, 4, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.WEST );
+
+    JGUIUtil.addComponent(main_JPanel, new JLabel ("Command:"), 
+		0, ++y, 1, 1, 0, 0, insetsTLBR, GridBagConstraints.NONE, GridBagConstraints.EAST);
+	__command_JTextArea = new JTextArea (4,40);
+	__command_JTextArea.setLineWrap ( true );
+	__command_JTextArea.setWrapStyleWord ( true );
+	__command_JTextArea.setEditable (false);
+	JGUIUtil.addComponent(main_JPanel, new JScrollPane(__command_JTextArea),
+		1, y, 6, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+	// Refresh the contents...
+	refresh ();
+
+	// South JPanel: North
+	JPanel button_JPanel = new JPanel();
+	button_JPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JGUIUtil.addComponent(main_JPanel, button_JPanel, 
+		0, ++y, 8, 1, 1, 0, insetsTLBR, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+ 
+	__cancel_JButton = new SimpleJButton("Cancel", this);
+	button_JPanel.add (__cancel_JButton);
+	__cancel_JButton.setToolTipText ( "Close window without saving changes." );
+	__ok_JButton = new SimpleJButton("OK", this);
+	button_JPanel.add (__ok_JButton);
+	__ok_JButton.setToolTipText ( "Close window and save changes to command." );
+
+	setTitle ( "Edit " + __command.getCommandName() + "() Command");
+	setResizable (false);
+    pack();
+    JGUIUtil.center(this);
+	refresh();	// Sets the __path_JButton status
+    super.setVisible(true);
+}
+
+/**
+Handle ItemEvent events.
+@param e ItemEvent to handle.
+*/
+public void itemStateChanged (ItemEvent e) {
+	refresh();
+}
+
+/**
+Respond to KeyEvents.
+*/
+public void keyPressed (KeyEvent event) {
+	int code = event.getKeyCode();
+
+	if (code == KeyEvent.VK_ENTER) {
+		refresh ();
+		checkInput();
+		if (!__error_wait) {
+			response ( true );
+		}
+	}
+}
+
+public void keyReleased (KeyEvent event) {
+	refresh();
+}
+
+public void keyTyped (KeyEvent event) {}
+
+/**
+Indicate if the user pressed OK (cancel otherwise).
+@return true if the edits were committed, false if the user cancelled.
+*/
+public boolean ok ()
+{	return __ok;
+}
+
+/**
+Refresh the command from the other text field contents.
+*/
+private void refresh ()
+{	String routine = getClass().getName() + ".refresh";
+    String TableID = "";
+    String NewTableID = "";
+    String IncludeColumns = "";
+	PropList props = __command.getCommandParameters();
+	if (__first_time) {
+		__first_time = false;
+        TableID = props.getValue ( "TableID" );
+        NewTableID = props.getValue ( "NewTableID" );
+        IncludeColumns = props.getValue ( "IncludeColumns" );
+        if ( TableID == null ) {
+            // Select default...
+            __TableID_JComboBox.select ( 0 );
+        }
+        else {
+            if ( JGUIUtil.isSimpleJComboBoxItem( __TableID_JComboBox,TableID, JGUIUtil.NONE, null, null ) ) {
+                __TableID_JComboBox.select ( TableID );
+            }
+            else {
+                Message.printWarning ( 1, routine,
+                "Existing command references an invalid\nTableID value \"" + TableID +
+                "\".  Select a different value or Cancel.");
+                __error_wait = true;
+            }
+        }
+        if ( NewTableID != null ) {
+            __NewTableID_JTextField.setText ( NewTableID );
+        }
+		if ( IncludeColumns != null ) {
+			__IncludeColumns_JTextField.setText ( IncludeColumns );
+		}
+	}
+	// Regardless, reset the command from the fields...
+	TableID = __TableID_JComboBox.getSelected();
+    NewTableID = __NewTableID_JTextField.getText().trim();
+	IncludeColumns = __IncludeColumns_JTextField.getText().trim();
+	props = new PropList ( __command.getCommandName() );
+    props.add ( "TableID=" + TableID );
+    props.add ( "NewTableID=" + NewTableID );
+	props.add ( "IncludeColumns=" + IncludeColumns );
+	__command_JTextArea.setText( __command.toString ( props ) );
+}
+
+/**
+React to the user response.
+@param ok if false, then the edit is canceled.  If true, the edit is committed and the dialog is closed.
+*/
+private void response ( boolean ok )
+{	__ok = ok;	// Save to be returned by ok()
+	if ( ok ) {
+		// Commit the changes...
+		commitEdits ();
+		if ( __error_wait ) {
+			// Not ready to close out!
+			return;
+		}
+	}
+	// Now close out...
+	setVisible( false );
+	dispose();
+}
+
+/**
+Responds to WindowEvents.
+@param event WindowEvent object 
+*/
+public void windowClosing(WindowEvent event) {
+	response ( false );
+}
+
+// The following methods are all necessary because this class implements WindowListener
+public void windowActivated(WindowEvent evt)	{}
+public void windowClosed(WindowEvent evt)	{}
+public void windowDeactivated(WindowEvent evt)	{}
+public void windowDeiconified(WindowEvent evt)	{}
+public void windowIconified(WindowEvent evt)	{}
+public void windowOpened(WindowEvent evt)	{}
+
+}

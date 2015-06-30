b963c2c9ae817965f4fe01800f2cfab7910abf5e
Steve Malers <steve.malers@openwaterfoundation.org>
2015-06-30 00:31:12 -0600

    Minor code cleanup.

--- src/rti/tscommandprocessor/commands/ts/RunningStatisticTimeSeries_Command.java
+++ src/rti/tscommandprocessor/commands/ts/RunningStatisticTimeSeries_Command.java
@@ -516,7 +516,7 @@ Run the command.
 public void runCommandInternal ( int command_number, CommandPhaseType commandPhase )
 throws InvalidCommandParameterException,
 CommandWarningException, CommandException
-{	String routine = getClass().getSimpleName() + ".runCommand", message;
+{	String routine = getClass().getSimpleName() + ".runCommandInternal", message;
 	int warning_level = 2;
 	String command_tag = "" + command_number;
 	int warning_count = 0;
